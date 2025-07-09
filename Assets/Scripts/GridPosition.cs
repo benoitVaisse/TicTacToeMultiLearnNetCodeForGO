1995a4b560ed4013f8d4107f89c8723cdf4508c0
benoit vaisse <benoit.vaisse@gmail.com>
2025-07-09 21:20:39 +0200

    gris setup

---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace TicTacToeMultiLearnNetCodeForGO.Assets.Scripts
+{
+    public class GridPosition : MonoBehaviour
+    {
+        [SerializeField]
+        private int x;
+        [SerializeField]
+        private int y;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnMouseDown()
+        {
+            Debug.Log("Click!!");
+        }
+    }
+}
