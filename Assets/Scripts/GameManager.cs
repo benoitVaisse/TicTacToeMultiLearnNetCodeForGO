355cbcdc5a0e0e6e87462305eea3d82088712d44
benoit vaisse <benoit.vaisse@gmail.com>
2025-07-09 23:04:08 +0200

    spwan cross test on server and client

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace TicTacToeMultiLearnNetCodeForGO.Assets.Scripts
 {
     public sealed class GameManager : MonoBehaviour
     {
         public static GameManager Instance { get; private set; }
+
+        public event EventHandler<OnClickOnGridPositionEventArgs> OnClickOnGridPosition;
+        public class OnClickOnGridPositionEventArgs : EventArgs
+        {
+            public int x;
+            public int y;
+        }
 
         public void Awake()
         {
@@ -18,6 +26,7 @@ namespace TicTacToeMultiLearnNetCodeForGO.Assets.Scripts
         public void ClikedOnGridPosition(int x, int y)
         {
             Debug.Log($"CLiked on position {x}, {y}");
+            OnClickOnGridPosition?.Invoke(this, new OnClickOnGridPositionEventArgs() { x = x, y = y });
         }
     }
 }
---
+++ Assets/Scripts/GameVisualManager.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace TicTacToeMultiLearnNetCodeForGO.Assets.Scripts
+{
+    public sealed class GameVisualManager : MonoBehaviour
+    {
+
+        private const float GRID_SIZE = 3.5f;
+        [SerializeField]
+        private Transform _crossPrebab;
+        [SerializeField]
+        private Transform _circlePrebab;
+
+
+        private void Start()
+        {
+            GameManager.Instance.OnClickOnGridPosition += Gamemanager_OnClickOnGridPosition;
+        }
+
+        private void Gamemanager_OnClickOnGridPosition(object sender, GameManager.OnClickOnGridPositionEventArgs e)
+        {
+            Transform spawnedCrossTransform = Instantiate(_crossPrebab, GetGrisWorldPosition(e.x, e.y), Quaternion.identity);
+            spawnedCrossTransform.GetComponent<NetworkObject>().Spawn(true);
+        }
+
+        private Vector2 GetGrisWorldPosition(int x, int y)
+            => new Vector2(-GRID_SIZE + (x * GRID_SIZE), -GRID_SIZE + (y * GRID_SIZE));
+    }
+}
