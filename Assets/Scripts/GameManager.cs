d5857bd80c1424a47f2d26b35a2083da80aabf08
benoit vaisse <benoit.vaisse@gmail.com>
2025-07-12 16:08:27 +0200

    InterfaceUI Player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,19 +8,21 @@ namespace TicTacToeMultiLearnNetCodeForGO.Assets.Scripts
     {
         public static GameManager Instance { get; private set; }
 
-        public event EventHandler<OnClickOnGridPositionEventArgs> OnClickOnGridPosition;
-
         private PlayerType _playerType;
         public PlayerType PlayerType => _playerType;
-        private PlayerType _currentPlayanlePlayerType;
+        private NetworkVariable<PlayerType> _currentPlayanlePlayerType = new();
+        public PlayerType CurrentPlayanlePlayerType => _currentPlayanlePlayerType.Value;
 
-
+        public event EventHandler<OnClickOnGridPositionEventArgs> OnClickOnGridPosition;
         public class OnClickOnGridPositionEventArgs : EventArgs
         {
             public int X;
             public int Y;
             public PlayerType PlayerType;
         }
+
+        public event EventHandler OnGameStartedEvent;
+        public event EventHandler OnSwitchCurrentPlayablePlayerEvent;
 
         public void Awake()
         {
@@ -39,30 +41,65 @@ namespace TicTacToeMultiLearnNetCodeForGO.Assets.Scripts
             else if (NetworkManager.Singleton.LocalClientId == 1)
                 _playerType = PlayerType.Circle;
 
+            Debug.Log($"Is Server : {IsServer}");
             if (IsServer)
-                _currentPlayanlePlayerType = PlayerType.Cross;
+            {
+                NetworkManager.Singleton.OnClientConnectedCallback += NetWorkManager_OnClientConnectedCallback;
+            }
+            OnCurrentPlayanlePlayerTypeValueChanged();
+        }
 
+        private void OnCurrentPlayanlePlayerTypeValueChanged()
+        {
+            _currentPlayanlePlayerType.OnValueChanged += (PlayerType oldValue, PlayerType newValue) =>
+            {
+                Debug.Log($"_currentPlayanlePlayerType had changed");
+                Debug.Log($"_currentPlayanlePlayerType old value : {oldValue}");
+                Debug.Log($"_currentPlayanlePlayerType new value : {newValue}");
+                OnSwitchCurrentPlayablePlayerEvent?.Invoke(this, EventArgs.Empty);
+            };
+        }
+
+        private void NetWorkManager_OnClientConnectedCallback(ulong obj)
+        {
+            if (NetworkManager.Singleton.ConnectedClientsList.Count == 2)
+            {
+                // start game
+                Debug.Log("Game Started");
+                _currentPlayanlePlayerType.Value = PlayerType.Cross;
+                TriggerOnGameStartedEventRpc();
+            }
+            else
+            {
+                Debug.Log($"missing {2 - NetworkManager.Singleton.ConnectedClientsList.Count} player(s)");
+            }
+        }
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void TriggerOnGameStartedEventRpc()
+        {
+            OnGameStartedEvent?.Invoke(this, EventArgs.Empty);
         }
 
         [Rpc(SendTo.Server)]
         public void ClikedOnGridPositionRpc(int x, int y, PlayerType localPlayerType)
         {
             Debug.Log($"CLiked on position {x}, {y}");
-            if (_currentPlayanlePlayerType != localPlayerType)
+            if (_currentPlayanlePlayerType.Value != localPlayerType)
             {
                 Debug.LogWarning($"Is not to {localPlayerType.ToString()} to play");
                 return;
             }
             OnClickOnGridPosition?.Invoke(this, new OnClickOnGridPositionEventArgs() { X = x, Y = y, PlayerType = localPlayerType });
 
-            switch (_currentPlayanlePlayerType)
+            switch (_currentPlayanlePlayerType.Value)
             {
                 default:
                 case PlayerType.Cross:
-                    _currentPlayanlePlayerType = PlayerType.Circle;
+                    _currentPlayanlePlayerType.Value = PlayerType.Circle;
                     break;
                 case PlayerType.Circle:
-                    _currentPlayanlePlayerType = PlayerType.Cross;
+                    _currentPlayanlePlayerType.Value = PlayerType.Cross;
                     break;
             }
         }
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace TicTacToeMultiLearnNetCodeForGO.Assets.Scripts
+{
+    public class PlayerUI : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _crossArrowGameObject;
+        [SerializeField]
+        private GameObject _circleArrowGameObject;
+        [SerializeField]
+        private GameObject _crossYouTextMesh;
+        [SerializeField]
+        private GameObject _circleYouTextMesh;
+
+        private void Awake()
+        {
+            _crossArrowGameObject.SetActive(false);
+            _circleArrowGameObject.SetActive(false);
+            _crossYouTextMesh.SetActive(false);
+            _circleYouTextMesh.SetActive(false);
+        }
+
+        private void Start()
+        {
+            GameManager.Instance.OnGameStartedEvent += GameManager_OnGameStarted;
+            GameManager.Instance.OnSwitchCurrentPlayablePlayerEvent += GameManager_OnSwitchCurrentPlayablePlayerEvent; ;
+        }
+
+        private void GameManager_OnSwitchCurrentPlayablePlayerEvent(object sender, System.EventArgs e)
+        {
+            UpdateUserPlayableArrow();
+        }
+
+        private void GameManager_OnGameStarted(object sender, System.EventArgs e)
+        {
+            if (GameManager.Instance.PlayerType == PlayerType.Cross)
+            {
+                _crossYouTextMesh.SetActive(true);
+            }
+            else
+            {
+                _circleYouTextMesh.SetActive(true);
+            }
+            UpdateUserPlayableArrow();
+        }
+
+        private void UpdateUserPlayableArrow()
+        {
+            Debug.Log($"UpdateUserPlayableArrow playertype {GameManager.Instance.PlayerType} {GameManager.Instance.CurrentPlayanlePlayerType}");
+            if (GameManager.Instance.CurrentPlayanlePlayerType == PlayerType.Cross)
+            {
+                _crossArrowGameObject.SetActive(true);
+                _circleArrowGameObject.SetActive(false);
+            }
+            else
+            {
+                _crossArrowGameObject.SetActive(false);
+                _circleArrowGameObject.SetActive(true);
+            }
+        }
+    }
+}
